9fdb5a5aabdd995d41890315c5c23fc414803d7f
Marcus Oates <moates695@gmail.com>
2024-09-26 15:44:50 +1000

    try new project name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="pglogging",
+    name="pglogginghandler",
     version="0.1",
     packages=find_packages(),
     install_requires=['psycopg2-binary==2.9.9'],
